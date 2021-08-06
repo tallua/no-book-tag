1828dca369aa80997fadbe38d99283d17ea6a574
tallua <talluay@gmail.com>
2021-08-07 00:34:35 +0900

    add download func

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,12 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import * as qs from 'qs';
 
-
 export function query_book_name(name: string): Promise<string[]> {
   return axios.get('https://www.nl.go.kr/NL/contents/search.do', {
     params: {
       pageNum: 1, pageSize: 30, srchTarget: 'total', kwd: name + '#'
     }
-  })
-    .then((data) => data.data)
+  }).then((data) => data.data)
     .then((data: string) => {
       const selector = cheerio.load(data);
       const targets = selector('a[href^="#viewKey"][title="새창열림"]');
@@ -18,10 +16,16 @@ export function query_book_name(name: string): Promise<string[]> {
       targets.each((i, elem) => {
         const href: string = `${elem.attribs['href']}`;
         const params = qs.parse(href);
-        result.push(`${params['#viewKey']}.${params['viewType']}`);
+        result.push(`${params['#viewKey']},${params['viewType']}`);
       });
 
       return result;
     })
 }
 
+
+export async function download_marc(book_id: string) {
+  return await axios.get('https://www.nl.go.kr/NL/marcDownload.do', {
+    params: { downData: book_id }
+  }).then((data) => data.data);
+}
